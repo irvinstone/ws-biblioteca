7215702c02643b73cf23cc17628d65ed993071ad
irvin <irvin.leon@comercio.com.pe>
2017-11-10 18:41:53 -0500

    feat(everithing) dejando listosss

--- app/routes/usuarioController.js
+++ app/routes/usuarioController.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var models = require('../models');
 var config = require('../config/config.json');
 var crypto = require('crypto');
+var usuarioService = require('../services/UserService');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -15,6 +16,12 @@ router.post('/', function(req, res, next) {
     models.usuario.create(req.body).then(function (result) {
         res.json(result)
     });
+});
+router.post('/auth', function(req, res, next) {
+    usuarioService.authenticar(req.body,function (err,success) {
+        if(err)res.json(err);
+        else res.json(success);
+    })
 });
 
 module.exports = router;
